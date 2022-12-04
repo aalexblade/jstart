91ba2b0863d8b6cca141fd7b5c1bd587e78ca892
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2022-12-04 09:39:14 +0200

    jstart

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // const validator = require("validator");
 
+// const { groupBy } = require("lodash");
+
 // const { reject } = require("lodash")
 
 // const { reject } = require("lodash");
@@ -77,16 +79,93 @@
 
 
 
-fetch("https://jsonplaceholder.typicode.com/users")
-.then(response =>{
-  if (!response.ok){
-    thow new Error(response.status);
-  }
-  return response.json();
-})
-then.(date=>{
+// fetch("https://jsonplaceholder.typicode.com/users")
+// .then(response =>{
+//   if (!response.ok){
+//     thow new Error(response.status);
+//   }
+//   return response.json();
+// })
+// then.(date=>{
 
-})
-.catch(error =>{
+// })
+// .catch(error =>{
   
-})
+// })
+
+// const fetchUsersBtn = document.querySelector(".btn");
+// const userList = document.querySelector(".user-list");
+
+// fetchUsersBtn.addEventListener("click", () => {
+//   fetchUsers()
+//     .then((users) => renderUserList(users))
+//     .catch((error) => console.log(error));
+// });
+
+// function fetchUsers() {
+//   return fetch("https://jsonplaceholder.typicode.com/users").then(
+//     (response) => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     }
+//   );
+// }
+
+// function renderUserList(users) {
+//   const markup = users
+//     .map((user) => {
+//       return `<li>
+//           <p><b>Name</b>: ${user.name}</p>
+//           <p><b>Email</b>: ${user.email}</p>
+//           <p><b>Company</b>: ${user.company.name}</p>
+//         </li>`;
+//     })
+//     .join("");
+//   userList.innerHTML = markup;
+// }
+
+// let globalVariable; // undefined
+
+// // Initializing data fetching
+// fetch("https://jsonplaceholder.typicode.com/users")
+//   .then(response => response.json())
+//   .then(users => {
+//     console.log("users inside then callback: ", users);
+
+//     // Writing the result to a global variable
+//     globalVariable = users;
+
+//     // Everything is ok here, the data is in the variable
+//     console.log("globalVariable inside fetch callback: ", globalVariable);
+//   });
+
+// // No async data here
+// console.log("globalVariable outside fetch: ", globalVariable); // undefined
+
+
+
+// const searchParams = new URLSearchParams({
+//   _limit: 5,
+//   _sort: "name",
+// });
+
+// console.log(searchParams.toString()); // "_limit=5&_sort=name"
+
+// const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+// console.log(url); // "https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name"
+
+
+// const headers = {
+//   "Content-Type": "application/json",
+//   "X-Custom-Header": "custom value",
+// };
+
+// fetch("https://jsonplaceholder.typicode.com/users", {
+//   headers: {
+//     Accept: "application/json",
+//   },
+// }).then(response => {
+//   // ...
+// });
