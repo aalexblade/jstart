af67446b3dc079ffd316578711d1c1070eda4c04
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2022-12-04 16:44:12 +0200

    jstart

--- index.js
+++ index.js
@@ -170,20 +170,83 @@
 //   // ...
 // });
 
+// const fetchPostsBtn = document.querySelector(".btn");
+// const userList = document.querySelector(".posts");
+
+// fetchPostsBtn.addEventListener("click", () => {
+//   fetchPosts()
+//     .then((posts) => renderPosts(posts))
+//     .catch((error) => console.log(error));
+// });
+
+// function fetchPosts() {
+//   const params = new URLSearchParams({
+//     _limit: 2,
+//     // Change the group number here
+//     _page: 5
+//   });
+
+//   return fetch(`https://jsonplaceholder.typicode.com/posts?${params}`).then(
+//     (response) => {
+//       if (!response.ok) {
+//         throw new Error(response.status);
+//       }
+//       return response.json();
+//     }
+//   );
+// }
+
+// function renderPosts(posts) {
+//   const markup = posts
+//     .map(({ id, title, body, userId }) => {
+//       return `<li>
+//           <h2 class="post-title">${title.slice(0, 30)}</h2>
+//           <p><b>Post id</b>: ${id}</p>
+//           <p><b>Author id</b>: ${userId}</p>
+//           <p class="post-body">${body}</p>
+//         </li>`;
+//     })
+//     .join("");
+//   userList.innerHTML = markup;
+// }
+
+
 const fetchPostsBtn = document.querySelector(".btn");
 const userList = document.querySelector(".posts");
+const alertPopup = document.querySelector(".alert");
+let isAlertVisible = false;
+
+// Controls the group number
+let page = 1;
+// Controls the number of items in the group
+let limit = 5;
+// In our case total number of pages is calculated on frontend
+const totalPages = 100 / limit;
 
 fetchPostsBtn.addEventListener("click", () => {
+  // Check the end of the collection to display an alert
+  if (page > totalPages) {
+    return toggleAlertPopup();
+  }
+
   fetchPosts()
-    .then((posts) => renderPosts(posts))
+    .then((posts) => {
+      renderPosts(posts);
+      // Increase the group number
+      page += 1;
+
+      // Replace button text after first request
+      if (page > 1) {
+        fetchPostsBtn.textContent = "Fetch more posts";
+      }
+    })
     .catch((error) => console.log(error));
 });
 
 function fetchPosts() {
   const params = new URLSearchParams({
-    _limit: 2,
-    // Change the group number here
-    _page: 5
+    _limit: limit,
+    _page: page
   });
 
   return fetch(`https://jsonplaceholder.typicode.com/posts?${params}`).then(
@@ -207,5 +270,17 @@ function renderPosts(posts) {
         </li>`;
     })
     .join("");
-  userList.innerHTML = markup;
+  userList.insertAdjacentHTML("beforeend", markup);
+}
+
+function toggleAlertPopup() {
+  if (isAlertVisible) {
+    return;
+  }
+  isAlertVisible = true;
+  alertPopup.classList.add("is-visible");
+  setTimeout(() => {
+    alertPopup.classList.remove("is-visible");
+    isAlertVisible = false;
+  }, 3000);
 }
