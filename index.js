d4c1345d00210b83b4a363e8384f7a81ceba83f9
Oleksandr Vasylenko <79399963+aalexblade@users.noreply.github.com>
2022-12-04 10:24:31 +0200

    jstart

--- index.js
+++ index.js
@@ -169,3 +169,43 @@
 // }).then(response => {
 //   // ...
 // });
+
+const fetchPostsBtn = document.querySelector(".btn");
+const userList = document.querySelector(".posts");
+
+fetchPostsBtn.addEventListener("click", () => {
+  fetchPosts()
+    .then((posts) => renderPosts(posts))
+    .catch((error) => console.log(error));
+});
+
+function fetchPosts() {
+  const params = new URLSearchParams({
+    _limit: 2,
+    // Change the group number here
+    _page: 5
+  });
+
+  return fetch(`https://jsonplaceholder.typicode.com/posts?${params}`).then(
+    (response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
+
+function renderPosts(posts) {
+  const markup = posts
+    .map(({ id, title, body, userId }) => {
+      return `<li>
+          <h2 class="post-title">${title.slice(0, 30)}</h2>
+          <p><b>Post id</b>: ${id}</p>
+          <p><b>Author id</b>: ${userId}</p>
+          <p class="post-body">${body}</p>
+        </li>`;
+    })
+    .join("");
+  userList.innerHTML = markup;
+}
